4594f137dc183033e240462440be7d73711aabf6
Amir Abozaid Abdelaziz <45122818+amirabozaid18@users.noreply.github.com>
2020-10-18 17:24:23 +0200

    Update LCD.c
    adding LCD_display_number() and power() functions

--- LCD.c
+++ LCD.c
@@ -1,11 +1,12 @@
 #include "LCD.h"
 
+
 uint8_t LINES_NUMBER, RS_PIN,	RW_PIN, E_PIN	;
 char DATA_PORT_DATA_R;
 char CONTROL_PORT_DATA_R;
-mode WORKING_MODE;
+size WORKING_MODE;
 
-void LCD_init(uint8_t lines_number, char data_port_name, char control_port_name, uint8_t rs_pin, uint8_t rw_pin, uint8_t e_pin, mode working_mode)
+void LCD_init(uint8_t lines_number, char data_port_name, char control_port_name, uint8_t rs_pin, uint8_t rw_pin, uint8_t e_pin, size working_mode)
 {
 	DATA_PORT_DATA_R	= data_port_name;
 	CONTROL_PORT_DATA_R = control_port_name;
@@ -42,7 +43,7 @@ void LCD_init(uint8_t lines_number, char data_port_name, char control_port_name,
 			LCD_command(0x28);											//2 lines 4-bits mode		
 
 	LCD_command(0x06);													//entry mode		
-	LCD_command(0x80)	;													//move cursor to beginning of first line	
+	LCD_command(0x80)	;												//move cursor to beginning of first line	
 	LCD_command(0x0E);													//Display on cursor on	
 	
 }
@@ -62,7 +63,7 @@ void LCD_command(uint8_t command)
 			delayMs(3);
 		else																										// any other command
 			delayUs(60);
-		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);							// ready for next command or data to be sent
+		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// ready for next command or data to be sent
 	}
 
 
@@ -76,10 +77,10 @@ void LCD_command(uint8_t command)
 			delayMs(3);
 		else																										// any other command
 			delayUs(60);
-		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);							// ready for next command or data to be sent
+		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// ready for next command or data to be sent
 		
 		
-		set_half_port(DATA_PORT_DATA_R,UPPER, command );				// send the lower half of command
+		set_half_port(DATA_PORT_DATA_R,UPPER, command );					// send the lower half of command
 		clear_output_pin(CONTROL_PORT_DATA_R,RS_PIN);						// send a command
 		clear_output_pin(CONTROL_PORT_DATA_R,RW_PIN);						//write to LCD
 		clear_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// send a higt-to-low pulse on enable pin
@@ -87,7 +88,7 @@ void LCD_command(uint8_t command)
 			delayMs(3);
 		else																										// any other command
 			delayUs(60);
-		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);							// ready for next command or data to be sent
+		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// ready for next command or data to be sent
 	}
 
 	
@@ -104,7 +105,7 @@ void LCD_char(uint8_t mychar)
 		clear_output_pin(CONTROL_PORT_DATA_R,RW_PIN);						//write to LCD
 		clear_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// send a higt-to-low pulse on enable pin
 		delayUs(60);
-		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);							// ready for next command or data to be sent
+		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// ready for next command or data to be sent
 	}
 		
 		
@@ -116,7 +117,7 @@ void LCD_char(uint8_t mychar)
 		clear_output_pin(CONTROL_PORT_DATA_R,RW_PIN);						//write to LCD
 		clear_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// send a higt-to-low pulse on enable pin
 		delayUs(60);
-		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);							// ready for next command or data to be sent
+		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// ready for next command or data to be sent
 
 		
 		set_half_port(DATA_PORT_DATA_R,UPPER,mychar);						// send the lower half of data
@@ -124,13 +125,13 @@ void LCD_char(uint8_t mychar)
 		clear_output_pin(CONTROL_PORT_DATA_R,RW_PIN);						//write to LCD
 		clear_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// send a higt-to-low pulse on enable pin
 		delayUs(60);
-		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);							// ready for next command or data to be sent
+		set_output_pin(CONTROL_PORT_DATA_R,E_PIN);						// ready for next command or data to be sent
 	}
 
 	
 }	
 
-void LCD_display(char* mystring)
+void LCD_display_string(char* mystring)
 {
 	uint8_t	display_position = 0;
 	LCD_command(0x01);																									//clear screen
@@ -138,7 +139,7 @@ void LCD_display(char* mystring)
 	{
 		LCD_char( *(mystring++) );
 		display_position++;
-		if ( (!(display_position %32))	&& 	LINES_NUMBER	== 2)					// 2 lines are full
+		if ( (!(display_position %32))	&& 	LINES_NUMBER	== 2)				// 2 lines are full
 		{
 			display_position = 0;
 			delayMs(3000);
@@ -160,5 +161,74 @@ void LCD_display(char* mystring)
 		}
 			
 	}
+}
+
+
+
+
+void LCD_display_number(int64_t mynumber)
+{
+	
+	uint8_t index = 0,length = 0, result = 0;
+	char converted_number [41];
+	
+	if (mynumber == 0)
+	{
+		*(converted_number) = '0';
+		index++;
+	}
+	else 
+	{
+		if ( mynumber < 0)
+		{
+			*(converted_number) = '-';
+			index++;
+			mynumber *= -1;
+		}
+		
+		while(1)
+		{
+			
+			result = mynumber / power(10,length); 
+			if ( result )
+				length++;
+			else		// result = 0
+				break;			
+		}
+		
+		while (length)
+		{
+			result = mynumber / power(10,length-1);
+			*(converted_number + index) = (char) ( result + 48);
+			index++;
+			mynumber -= (result * power(10,length-1) );
+			length--;
+		}
+		
+	}
+		
+	*(converted_number + index) = '\0';
+	
+	LCD_display_string(converted_number);
+		
+	
+	
+}
+
+
+uint64_t power(uint32_t base, uint8_t exponent)
+{
+	uint64_t result =1;
+	uint8_t k;
+	if (exponent == 0)
+			return 1;
+	
+	else
+	{
+		for (k=1;k<=exponent;k++)
+			result *= base;
+	}
+	
+	return result;
 }
 
