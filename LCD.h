70ef12531f72c9657512b2de36b542ca67d538b4
Amir Abozaid Abdelaziz <45122818+amirabozaid18@users.noreply.github.com>
2020-10-18 17:25:57 +0200

    Update LCD.h
    adding the prototypes of LCD_display_number() and power() functions

--- LCD.h
+++ LCD.h
@@ -2,15 +2,19 @@
 #define LCD_H
 
 #include "DIO.h"
-typedef enum LCD_mode	{ FOUR_BITS, EIGHT_BITS} mode;
+
+typedef enum LCD_mode	{ FOUR_BITS, EIGHT_BITS} size;
 
 
-void LCD_init(uint8_t lines_number, char data_port_name, char control_port_name, uint8_t rs_pin, uint8_t rw_pin, uint8_t e_pin, mode working_mode);
+void LCD_init(uint8_t lines_number, char data_port_name, char control_port_name, uint8_t rs_pin, uint8_t rw_pin, uint8_t e_pin, size working_mode);
 void LCD_command(uint8_t command);
 void LCD_char(uint8_t mychar);
-void LCD_display(char* mystring);
+void LCD_display_string(char* mystring);
+void LCD_display_number(int64_t mynumber);
 
 
+
+uint64_t power(uint32_t base, uint8_t exponent);
 
 
 #endif
