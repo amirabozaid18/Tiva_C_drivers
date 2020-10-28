fcd58a1e154abc91fbcda516d8ae2b8856149eb9
Amir Abozaid Abdelaziz <45122818+amirabozaid18@users.noreply.github.com>
2020-10-28 10:15:46 +0200

    add disable_UART()

--- UART.h
+++ UART.h
@@ -16,6 +16,7 @@ void set_Baud_rate(uint8_t UART_number,uint32_t Baud_Rate, uint32_t system_clock
 void configure_UART(uint8_t UART_number, length data_length, usage two_stop_bits, usage parity_check, sort parity_sort);
 void set_UART_interrupts(uint8_t UART_number, status reception_interrupt, status transmission_interrupt, status frame_error_interrupt , status parity_error_interrupt, status break_error_interrupt, status overrun_error_interrupt, uint8_t interrupt_priority);
 void run_UART(uint8_t UART_number);
+void disable_UART(uint8_t UART_number);
 void UART_send_char(uint8_t UART_number, uint8_t mydata);
 void UART_send_string(uint8_t UART_number, char* mystring);
 void UART_send_number(uint8_t UART_number,int64_t mynumber);
