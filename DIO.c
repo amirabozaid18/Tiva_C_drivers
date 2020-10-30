bf123c0ce463aed3bbe3dca769aef107ed162aab
Amir Abozaid Abdelaziz <45122818+amirabozaid18@users.noreply.github.com>
2020-10-30 18:24:21 +0200

    add trigger_external_interrupt() and power()

--- DIO.c
+++ DIO.c
@@ -38,20 +38,20 @@ void initialize_port(char port_name, type port_type)
 		
 	}
 	
-	else if (port_name == 'C')					// this port is special because pins 0-3 are JTAG pins so we avoid them
+	else if (port_name == 'C')
 	{
 		SYSCTL_RCGCGPIO_R |= (1<<2);			// connect clock to PORTC
 		while ( (SYSCTL_PRGPIO_R & (1<<2) ) == 0);		// check if clock is connected to PORTC
-		GPIO_PORTC_AFSEL_R &= 0x0F;						// pins 4-7 in PORTC are used as digital inputs/outputs
-		GPIO_PORTC_AMSEL_R &= 0x0F;						// pins 4-7 in PORTC are used as digital inputs/outputs
-		GPIO_PORTC_DEN_R	 |= 0xF0;						// pins 4-7 in PORTC are used as digital inputs/outputs
+		GPIO_PORTC_AFSEL_R = 0;						// all pins in PORTC are used as digital inputs/outputs
+		GPIO_PORTC_AMSEL_R = 0;						// all pins in PORTC are used as digital inputs/outputs
+		GPIO_PORTC_DEN_R	 = 0xFF;				// all pins in PORTC are used as digital inputs/outputs
 		if (port_type == INPUT)
 		{
-			GPIO_PORTC_DIR_R &= 0x0F;				// pins 4-7 in PORTC are inputs
-			GPIO_PORTC_PUR_R |= 0xF0;				// pull-up resistors are enabled for pins 4-7 in PORTC
+			GPIO_PORTC_DIR_R = 0x00;				// all pins in PORTC are inputs
+			GPIO_PORTC_PUR_R = 0xFF;				// pull-up resistors are enabled for all PORTC pins
 		}
 		else
-			GPIO_PORTC_DIR_R |= 0xF0;				// pins 4-7 in PORTC are outputs
+			GPIO_PORTC_DIR_R = 0xFF;				// all pins in PORTC are outputs
 		
 	}
 	
@@ -123,7 +123,7 @@ void set_output_port(char port_name,uint8_t port_value)
 		GPIO_PORTB_DATA_R = port_value;
 	
 	else if (port_name == 'C')
-		GPIO_PORTC_DATA_R = ( (GPIO_PORTC_DATA_R & (0x0F) ) | (port_value << 4) ); 					// because pins 0-3 in port C are JTAG pins
+		GPIO_PORTC_DATA_R = port_value; 					
 	
 	else if (port_name == 'D')
 		GPIO_PORTD_DATA_R = port_value;
@@ -156,8 +156,8 @@ void set_half_port(char port_name, location half_port_location, uint8_t half_por
 	
 	else if (port_name == 'C')
 	{
-		/*if (half_port_location  == LOWER)														// forbidden since pins 0-3 are used for JTAG
-			GPIO_PORTA_DATA_R = ( GPIO_PORTA_DATA_R & 0xF0) | ( half_port_value & 0x0F);*/
+		if (half_port_location  == LOWER)											
+			GPIO_PORTA_DATA_R = ( GPIO_PORTA_DATA_R & 0xF0) | ( half_port_value & 0x0F);
 		if (half_port_location  == UPPER)	
 			GPIO_PORTC_DATA_R = ( GPIO_PORTC_DATA_R & 0x0F) | ( half_port_value << 4);
 	}
@@ -197,7 +197,7 @@ void clear_output_port(char port_name)
 		GPIO_PORTB_DATA_R = 0;
 	
 	else if (port_name == 'C')
-		GPIO_PORTC_DATA_R &= 0x0F;					// because pins 0-3 in port C are JTAG pins
+		GPIO_PORTC_DATA_R = 0;
 	
 	else if (port_name == 'D')
 		GPIO_PORTD_DATA_R = 0;
@@ -230,8 +230,8 @@ void clear_half_port(char port_name, location half_port_location)
 	
 	else if (port_name == 'C')
 	{
-		/*if ( half_port_location == LOWER)			// forbidden since pins 0-3 are used for JTAG
-			GPIO_PORTA_DATA_R &= 0xF0;*/
+		if ( half_port_location == LOWER)
+			GPIO_PORTA_DATA_R &= 0xF0;
 		if ( half_port_location == UPPER)
 			GPIO_PORTC_DATA_R &= 0x0F;
 	}
@@ -270,16 +270,16 @@ void update_port_except_last_pin(char port_name, uint8_t updated_value)
 		GPIO_PORTB_DATA_R		= (GPIO_PORTB_DATA_R & 0x80) | ( updated_value & 0x7F);
 	
 	else if (port_name  == 'C')
-		GPIO_PORTC_DATA_R		= (GPIO_PORTC_DATA_R & 0x8F) | ( updated_value & 0x70);		// since pins 0-3 are used for JTAG pins
+		GPIO_PORTC_DATA_R		= (GPIO_PORTC_DATA_R & 0x80) | ( updated_value & 0x7F);
 	
 	else if (port_name  == 'D')
 		GPIO_PORTD_DATA_R		= (GPIO_PORTD_DATA_R & 0x80) | ( updated_value & 0x7F);
 	
 	else if (port_name  == 'E')
-		GPIO_PORTE_DATA_R		= (GPIO_PORTE_DATA_R & 0x80) | ( updated_value & 0x7F);
+		GPIO_PORTE_DATA_R		= (GPIO_PORTE_DATA_R & 0x20) | ( updated_value & 0x1F);
 	
 	else if (port_name  == 'F')
-		GPIO_PORTF_DATA_R		= (GPIO_PORTF_DATA_R & 0x80) | ( updated_value & 0x7F);
+		GPIO_PORTF_DATA_R		= (GPIO_PORTF_DATA_R & 0x10) | ( updated_value & 0x0F);
 }
 
 
@@ -292,16 +292,16 @@ void update_port_except_last_two_pins(char port_name, uint8_t updated_value)
 		GPIO_PORTB_DATA_R		= (GPIO_PORTB_DATA_R & 0xC0) | ( updated_value & 0x3F);
 	
 	else if (port_name  == 'C')
-		GPIO_PORTC_DATA_R		= (GPIO_PORTC_DATA_R & 0xCF) | ( updated_value & 0x30);		// since pins 0-3 are used for JTAG pins
+		GPIO_PORTC_DATA_R		= (GPIO_PORTC_DATA_R & 0xC0) | ( updated_value & 0x3F);
 	
 	else if (port_name  == 'D')
 		GPIO_PORTD_DATA_R		= (GPIO_PORTD_DATA_R & 0xC0) | ( updated_value & 0x3F);
 	
 	else if (port_name  == 'E')
-		GPIO_PORTE_DATA_R		= (GPIO_PORTE_DATA_R & 0xC0) | ( updated_value & 0x3F);
+		GPIO_PORTE_DATA_R		= (GPIO_PORTE_DATA_R & 0x30) | ( updated_value & 0x0F);
 	
 	else if (port_name  == 'F')
-		GPIO_PORTF_DATA_R		= (GPIO_PORTF_DATA_R & 0xC0) | ( updated_value & 0x3F);
+		GPIO_PORTF_DATA_R		= (GPIO_PORTF_DATA_R & 0x18) | ( updated_value & 0x07);
 }
 
 
@@ -314,7 +314,7 @@ uint8_t is_whole_port_output(char port_name)
 		return ( (GPIO_PORTB_DIR_R & (0xFF) ) == 0xFF );
 	
 	else if (port_name == 'C')
-		return ( (GPIO_PORTC_DIR_R & (0xF0) ) == 0xF0 );		// we check pins 4-7 only since pins 0-3 are JTAG pins
+		return ( (GPIO_PORTC_DIR_R & (0xFF) ) == 0xFF );
 	
 	else if (port_name == 'D')
 		return ( (GPIO_PORTD_DIR_R & (0xFF) ) == 0xFF );
@@ -324,6 +324,8 @@ uint8_t is_whole_port_output(char port_name)
 	
 	else if (port_name == 'F')
 		return ( (GPIO_PORTF_DIR_R & (0x1F) ) == 0x1F );		// because PORTF has only 5 bits
+	
+	return 0xFF;
 	
 	
 }
@@ -337,7 +339,7 @@ uint8_t is_whole_port_input(char port_name)				// we care about first 8 bits onl
 		return ( ((GPIO_PORTB_DIR_R & (0xFF)) | (0x00) ) == 0x00 );
 	
 	else if (port_name == 'C')
-		return ( ((GPIO_PORTC_DIR_R & (0xFF)) | (0x0F) ) == 0x0F );		// we check pins 4-7 only since pins 0-3 are JTAG pins
+		return ( ((GPIO_PORTC_DIR_R & (0xFF)) | (0x00) ) == 0x00 );
 	
 	else if (port_name == 'D')
 		return ( ((GPIO_PORTD_DIR_R & (0xFF)) | (0x00) ) == 0x00 );
@@ -347,6 +349,8 @@ uint8_t is_whole_port_input(char port_name)				// we care about first 8 bits onl
 	
 	else if (port_name == 'F')
 		return ( ((GPIO_PORTF_DIR_R & (0xFF)) | (0xE0) ) == 0xE0 );		// because PORTF has only 5 bits
+	
+	return 0xFF;
 	
 	
 }
@@ -359,7 +363,7 @@ uint8_t read_input_port(char port_name)				// we care about first 8 bits only si
 		return (GPIO_PORTB_DATA_R & 0xFF) ;
 	
 	else if (port_name == 'C')
-		return (GPIO_PORTC_DATA_R & 0xF0) ;
+		return (GPIO_PORTC_DATA_R & 0xFF) ;
 	
 	else if (port_name == 'D')
 		return (GPIO_PORTD_DATA_R & 0xFF) ;
@@ -372,7 +376,7 @@ uint8_t read_input_port(char port_name)				// we care about first 8 bits only si
 }
 
 
-void initialize_pin(char port_name, uint8_t pin_number , type pin_type)					// this function is used if the port containing this pin is not initialized
+void initialize_pin(char port_name, uint8_t pin_number , type pin_type)					
 {
 	if (port_name == 'A')
 	{
@@ -486,7 +490,7 @@ void initialize_pin(char port_name, uint8_t pin_number , type pin_type)					// t
 }
 
 
-void change_pin_direction (char port_name, uint8_t pin_number, type pin_type)			// this function must be used after initialize_port() only if port is defined as output and you want to define a pin it this port as input and vice versa
+void change_pin_direction (char port_name, uint8_t pin_number, type pin_type)			
 {
 	if ( port_name == 'A' )
 	{
@@ -583,7 +587,118 @@ void change_pin_direction (char port_name, uint8_t pin_number, type pin_type)			
 	
 }
 
-void set_output_pin(char port_name, uint8_t pin_number)		// this function must be used after change_pin_direction() only if this pin is in a port which was defined as input port, else you can call it directly after port initiallization
+void set_pin_alternative_function(char port_name,uint8_t pin_number,uint8_t control_value)			
+{
+	switch (port_name)
+	{
+		case 'A' :
+		{
+			GPIO_PORTA_AFSEL_R	|= (1<< pin_number);
+			switch (pin_number)
+			{
+				case 0:	GPIO_PORTA_PCTL_R	= (GPIO_PORTA_PCTL_R & 0xFFFFFFF0)	| (control_value << 0);	break;
+				case 1:	GPIO_PORTA_PCTL_R	= (GPIO_PORTA_PCTL_R & 0xFFFFFF0F)	| (control_value << 4);	break;
+				case 2:	GPIO_PORTA_PCTL_R	= (GPIO_PORTA_PCTL_R & 0xFFFFF0FF)	| (control_value << 8);	break;
+				case 3:	GPIO_PORTA_PCTL_R	= (GPIO_PORTA_PCTL_R & 0xFFFF0FFF)	| (control_value << 12);	break;
+				case 4:	GPIO_PORTA_PCTL_R	= (GPIO_PORTA_PCTL_R & 0xFFF0FFFF)	| (control_value << 16);	break;
+				case 5:	GPIO_PORTA_PCTL_R	= (GPIO_PORTA_PCTL_R & 0xFF0FFFFF)	| (control_value << 20);	break;
+				case 6:	GPIO_PORTA_PCTL_R	= (GPIO_PORTA_PCTL_R & 0xF0FFFFFF)	| (control_value << 24);	break;
+				case 7:	GPIO_PORTA_PCTL_R	= (GPIO_PORTA_PCTL_R & 0x0FFFFFFF)	| (control_value << 28);	break;
+				default: break;
+			}
+			break;
+		}
+		
+		case 'B' :
+		{
+			GPIO_PORTB_AFSEL_R	|= (1<< pin_number);
+			switch (pin_number)
+			{
+				case 0:	GPIO_PORTB_PCTL_R	= (GPIO_PORTB_PCTL_R & 0xFFFFFFF0)	| (control_value << 0);	break;
+				case 1:	GPIO_PORTB_PCTL_R	= (GPIO_PORTB_PCTL_R & 0xFFFFFF0F)	| (control_value << 4);	break;
+				case 2:	GPIO_PORTB_PCTL_R	= (GPIO_PORTB_PCTL_R & 0xFFFFF0FF)	| (control_value << 8);	break;
+				case 3:	GPIO_PORTB_PCTL_R	= (GPIO_PORTB_PCTL_R & 0xFFFF0FFF)	| (control_value << 12);	break;
+				case 4:	GPIO_PORTB_PCTL_R	= (GPIO_PORTB_PCTL_R & 0xFFF0FFFF)	| (control_value << 16);	break;
+				case 5:	GPIO_PORTB_PCTL_R	= (GPIO_PORTB_PCTL_R & 0xFF0FFFFF)	| (control_value << 20);	break;
+				case 6:	GPIO_PORTB_PCTL_R	= (GPIO_PORTB_PCTL_R & 0xF0FFFFFF)	| (control_value << 24);	break;
+				case 7:	GPIO_PORTB_PCTL_R	= (GPIO_PORTB_PCTL_R & 0x0FFFFFFF)	| (control_value << 28);	break;
+				default: break;
+			}
+			break;
+		}
+		
+		case 'C' :
+		{
+			GPIO_PORTC_AFSEL_R	|= (1<< pin_number);
+			switch (pin_number)
+			{
+				case 0:	GPIO_PORTC_PCTL_R	= (GPIO_PORTC_PCTL_R & 0xFFFFFFF0)	| (control_value << 0);	break;
+				case 1:	GPIO_PORTC_PCTL_R	= (GPIO_PORTC_PCTL_R & 0xFFFFFF0F)	| (control_value << 4);	break;
+				case 2:	GPIO_PORTC_PCTL_R	= (GPIO_PORTC_PCTL_R & 0xFFFFF0FF)	| (control_value << 8);	break;
+				case 3:	GPIO_PORTC_PCTL_R	= (GPIO_PORTC_PCTL_R & 0xFFFF0FFF)	| (control_value << 12);	break;
+				case 4:	GPIO_PORTC_PCTL_R	= (GPIO_PORTC_PCTL_R & 0xFFF0FFFF)	| (control_value << 16);	break;
+				case 5:	GPIO_PORTC_PCTL_R	= (GPIO_PORTC_PCTL_R & 0xFF0FFFFF)	| (control_value << 20);	break;
+				case 6:	GPIO_PORTC_PCTL_R	= (GPIO_PORTC_PCTL_R & 0xF0FFFFFF)	| (control_value << 24);	break;
+				case 7:	GPIO_PORTC_PCTL_R	= (GPIO_PORTC_PCTL_R & 0x0FFFFFFF)	| (control_value << 28);	break;
+				default: break;
+			}
+			break;
+		}
+		
+		case 'D' :
+		{
+			GPIO_PORTD_AFSEL_R	|= (1<< pin_number);
+			switch (pin_number)
+			{
+				case 0:	GPIO_PORTD_PCTL_R	= (GPIO_PORTD_PCTL_R & 0xFFFFFFF0)	| (control_value << 0);	break;
+				case 1:	GPIO_PORTD_PCTL_R	= (GPIO_PORTD_PCTL_R & 0xFFFFFF0F)	| (control_value << 4);	break;
+				case 2:	GPIO_PORTD_PCTL_R	= (GPIO_PORTD_PCTL_R & 0xFFFFF0FF)	| (control_value << 8);	break;
+				case 3:	GPIO_PORTD_PCTL_R	= (GPIO_PORTD_PCTL_R & 0xFFFF0FFF)	| (control_value << 12);	break;
+				case 4:	GPIO_PORTD_PCTL_R	= (GPIO_PORTD_PCTL_R & 0xFFF0FFFF)	| (control_value << 16);	break;
+				case 5:	GPIO_PORTD_PCTL_R	= (GPIO_PORTD_PCTL_R & 0xFF0FFFFF)	| (control_value << 20);	break;
+				case 6:	GPIO_PORTD_PCTL_R	= (GPIO_PORTD_PCTL_R & 0xF0FFFFFF)	| (control_value << 24);	break;
+				case 7:	GPIO_PORTD_PCTL_R	= (GPIO_PORTD_PCTL_R & 0x0FFFFFFF)	| (control_value << 28);	break;
+				default: break;
+			}
+			break;
+		}
+		
+		case 'E' :
+		{
+			GPIO_PORTE_AFSEL_R	|= (1<< pin_number);
+			switch (pin_number)
+			{
+				case 0:	GPIO_PORTE_PCTL_R	= (GPIO_PORTE_PCTL_R & 0xFFFFFFF0)	| (control_value << 0);	break;
+				case 1:	GPIO_PORTE_PCTL_R	= (GPIO_PORTE_PCTL_R & 0xFFFFFF0F)	| (control_value << 4);	break;
+				case 2:	GPIO_PORTE_PCTL_R	= (GPIO_PORTE_PCTL_R & 0xFFFFF0FF)	| (control_value << 8);	break;
+				case 3:	GPIO_PORTE_PCTL_R	= (GPIO_PORTE_PCTL_R & 0xFFFF0FFF)	| (control_value << 12);	break;
+				case 4:	GPIO_PORTE_PCTL_R	= (GPIO_PORTE_PCTL_R & 0xFFF0FFFF)	| (control_value << 16);	break;
+				case 5:	GPIO_PORTE_PCTL_R	= (GPIO_PORTE_PCTL_R & 0xFF0FFFFF)	| (control_value << 20);	break;
+				default: break;
+			}
+			break;
+		}
+		
+		case 'F' :
+		{
+			GPIO_PORTF_AFSEL_R	|= (1<< pin_number);
+			switch (pin_number)
+			{
+				case 0:	GPIO_PORTF_PCTL_R	= (GPIO_PORTF_PCTL_R & 0xFFFFFFF0)	| (control_value << 0);	break;
+				case 1:	GPIO_PORTF_PCTL_R	= (GPIO_PORTF_PCTL_R & 0xFFFFFF0F)	| (control_value << 4);	break;
+				case 2:	GPIO_PORTF_PCTL_R	= (GPIO_PORTF_PCTL_R & 0xFFFFF0FF)	| (control_value << 8);	break;
+				case 3:	GPIO_PORTF_PCTL_R	= (GPIO_PORTF_PCTL_R & 0xFFFF0FFF)	| (control_value << 12);	break;
+				case 4:	GPIO_PORTF_PCTL_R	= (GPIO_PORTF_PCTL_R & 0xFFF0FFFF)	| (control_value << 16);	break;
+				default: break;
+			}
+			break;
+		}
+		
+		default: break;
+	}
+}
+
+void set_output_pin(char port_name, uint8_t pin_number)		
 {
 	if (port_name == 'A')
 		GPIO_PORTA_DATA_R |= (1<<pin_number);
@@ -605,7 +720,7 @@ void set_output_pin(char port_name, uint8_t pin_number)		// this function must b
 }
 
 
-void clear_output_pin(char port_name, uint8_t pin_number)		// this function must be used after change_pin_direction() only if this pin is in a port which was defined as input port, else you can call it directly after port initiallization
+void clear_output_pin(char port_name, uint8_t pin_number)		
 {
 	if (port_name == 'A')
 		GPIO_PORTA_DATA_R &= ~(1<<pin_number);
@@ -626,7 +741,7 @@ void clear_output_pin(char port_name, uint8_t pin_number)		// this function must
 		GPIO_PORTF_DATA_R &= ~(1<<pin_number);
 }
 
-void toggle_output_pin(char port_name, uint8_t pin_number)		// this function must be used after change_pin_direction() only if this pin is in a port which was defined as input port, else you can call it directly after port initiallization
+void toggle_output_pin(char port_name, uint8_t pin_number)		
 {
 	if (port_name == 'A')
 		GPIO_PORTA_DATA_R ^= (1<<pin_number);
@@ -669,7 +784,192 @@ uint8_t read_input_pin(char port_name, uint8_t pin_number)
 		return ( ( GPIO_PORTF_DATA_R & (1<<pin_number) ) == (1<<pin_number) );
 }
 
-void delayMs(int n)
+
+void trigger_external_interrupt(char port_name, uint8_t pin_number, sensitivity interrupt_sensitivity, count edge_count, EVENT interrupt_event, uint8_t interrupt_priority)
+{
+	switch (port_name)
+	{
+		case 'A': 
+		{
+			GPIO_PORTA_ICR_R |= (1<<pin_number);
+			GPIO_PORTA_IM_R |= (1<<pin_number);
+			
+			NVIC_EN0_R |= (1<<0);
+			NVIC_PRI0_R = ( NVIC_PRI0_R & 0xFFFFFF00) | ( interrupt_priority<<5);
+			
+			if (interrupt_sensitivity == LEVEL)
+				GPIO_PORTA_IS_R |= (1<<pin_number);
+			else 			// 	EDGE sensitive
+				GPIO_PORTA_IS_R &=~ (1<<pin_number);
+			
+			if (edge_count == BOTH)
+				GPIO_PORTA_IBE_R |= (1<<pin_number);
+			else 		// SINGLE
+			{
+				GPIO_PORTA_IBE_R &=~ (1<<pin_number);
+				
+				if (interrupt_event == RISING)		// HIGH LEVEL or RISING EDGE
+					GPIO_PORTA_IEV_R |= (1<<pin_number);
+				else		// LOW LEVEL or FALLING EDGE
+					GPIO_PORTA_IEV_R &=~ (1<<pin_number);
+			}
+				
+			
+		__enable_irq();
+			break;
+		}
+		case 'B': 
+		{
+			GPIO_PORTB_ICR_R |= (1<<pin_number);
+			GPIO_PORTB_IM_R |= (1<<pin_number);
+			
+			NVIC_EN0_R |= (1<<1);
+			NVIC_PRI0_R = ( NVIC_PRI0_R & 0xFFFF00FF) | ( interrupt_priority<<13);
+			
+			if (interrupt_sensitivity == LEVEL)
+				GPIO_PORTB_IS_R |= (1<<pin_number);
+			else 			// 	EDGE sensitive
+				GPIO_PORTB_IS_R &=~ (1<<pin_number);
+			
+			if (edge_count == BOTH)
+				GPIO_PORTB_IBE_R |= (1<<pin_number);
+			else 		// SINGLE
+			{
+				GPIO_PORTB_IBE_R &=~ (1<<pin_number);
+				
+				if (interrupt_event == RISING)		// HIGH LEVEL or RISING EDGE
+					GPIO_PORTB_IEV_R |= (1<<pin_number);
+				else		// LOW LEVEL or FALLING EDGE
+					GPIO_PORTB_IEV_R &=~ (1<<pin_number);
+			}
+				
+			
+		__enable_irq();
+			break;
+		}
+		case 'C': 
+		{
+			GPIO_PORTC_ICR_R |= (1<<pin_number);
+			GPIO_PORTC_IM_R |= (1<<pin_number);
+			
+			NVIC_EN0_R |= (1<<2);
+			NVIC_PRI0_R = ( NVIC_PRI0_R & 0xFF00FFFF) | ( interrupt_priority<<21);
+			
+			if (interrupt_sensitivity == LEVEL)
+				GPIO_PORTC_IS_R |= (1<<pin_number);
+			else 			// 	EDGE sensitive
+				GPIO_PORTC_IS_R &=~ (1<<pin_number);
+			
+			if (edge_count == BOTH)
+				GPIO_PORTC_IBE_R |= (1<<pin_number);
+			else 		// SINGLE
+			{
+				GPIO_PORTC_IBE_R &=~ (1<<pin_number);
+				
+				if (interrupt_event == RISING)		// HIGH LEVEL or RISING EDGE
+					GPIO_PORTC_IEV_R |= (1<<pin_number);
+				else		// LOW LEVEL or FALLING EDGE
+					GPIO_PORTC_IEV_R &=~ (1<<pin_number);
+			}
+				
+			
+		__enable_irq();
+			break;
+		}
+		case 'D': 
+		{
+			GPIO_PORTD_ICR_R |= (1<<pin_number);
+			GPIO_PORTD_IM_R |= (1<<pin_number);
+			
+			NVIC_EN0_R |= (1<<3);
+			NVIC_PRI0_R = ( NVIC_PRI0_R & 0x00FFFFFF) | ( interrupt_priority<<29);
+			
+			if (interrupt_sensitivity == LEVEL)
+				GPIO_PORTD_IS_R |= (1<<pin_number);
+			else 			// 	EDGE sensitive
+				GPIO_PORTD_IS_R &=~ (1<<pin_number);
+			
+			if (edge_count == BOTH)
+				GPIO_PORTD_IBE_R |= (1<<pin_number);
+			else 		// SINGLE
+			{
+				GPIO_PORTD_IBE_R &=~ (1<<pin_number);
+				
+				if (interrupt_event == RISING)		// HIGH LEVEL or RISING EDGE
+					GPIO_PORTD_IEV_R |= (1<<pin_number);
+				else		// LOW LEVEL or FALLING EDGE
+					GPIO_PORTD_IEV_R &=~ (1<<pin_number);
+			}
+				
+			
+		__enable_irq();
+			break;
+		}
+		case 'E': 
+		{
+			GPIO_PORTE_ICR_R |= (1<<pin_number);
+			GPIO_PORTE_IM_R |= (1<<pin_number);
+			
+			NVIC_EN0_R |= (1<<4);
+			NVIC_PRI1_R = ( NVIC_PRI1_R & 0xFFFFFF00) | ( interrupt_priority<<5);
+			
+			if (interrupt_sensitivity == LEVEL)
+				GPIO_PORTE_IS_R |= (1<<pin_number);
+			else 			// 	EDGE sensitive
+				GPIO_PORTE_IS_R &=~ (1<<pin_number);
+			
+			if (edge_count == BOTH)
+				GPIO_PORTE_IBE_R |= (1<<pin_number);
+			else 		// SINGLE
+			{
+				GPIO_PORTE_IBE_R &=~ (1<<pin_number);
+				
+				if (interrupt_event == RISING)		// HIGH LEVEL or RISING EDGE
+					GPIO_PORTE_IEV_R |= (1<<pin_number);
+				else		// LOW LEVEL or FALLING EDGE
+					GPIO_PORTE_IEV_R &=~ (1<<pin_number);
+			}
+				
+			
+		__enable_irq();
+			break;
+		}
+		case 'F': 
+		{
+			GPIO_PORTF_ICR_R |= (1<<pin_number);
+			GPIO_PORTF_IM_R |= (1<<pin_number);
+			
+			NVIC_EN0_R |= (1<<30);
+			NVIC_PRI7_R = ( NVIC_PRI7_R & 0xFF00FFFF) | ( interrupt_priority<<21);
+			
+			if (interrupt_sensitivity == LEVEL)
+				GPIO_PORTF_IS_R |= (1<<pin_number);
+			else 			// 	EDGE sensitive
+				GPIO_PORTF_IS_R &=~ (1<<pin_number);
+			
+			if (edge_count == BOTH)
+				GPIO_PORTF_IBE_R |= (1<<pin_number);
+			else 		// SINGLE
+			{
+				GPIO_PORTF_IBE_R &=~ (1<<pin_number);
+				
+				if (interrupt_event == RISING)		// HIGH LEVEL or RISING EDGE
+					GPIO_PORTF_IEV_R |= (1<<pin_number);
+				else		// LOW LEVEL or FALLING EDGE
+					GPIO_PORTF_IEV_R &=~ (1<<pin_number);
+			}
+				
+			
+		__enable_irq();
+			break;
+		}
+		default: break;
+	}
+	
+}
+
+
+void delayMs(uint64_t n)
 {
 int i, j;
 for(i = 0 ; i < n; i++)
@@ -677,10 +977,27 @@ for(i = 0 ; i < n; i++)
 
 }
 
-void delayUs(int n)
+void delayUs(uint64_t n)
 {
 int i, j;
 for(i = 0 ; i < n; i++)
 	for(j = 0; j < 3; j++);
 
+}
+
+
+uint64_t power(uint32_t base, uint8_t exponent)
+{
+	uint64_t result =1;
+	uint8_t k;
+	if (exponent == 0)
+			return 1;
+	
+	else
+	{
+		for (k=1;k<=exponent;k++)
+			result *= base;
+	}
+	
+	return result;
 }
